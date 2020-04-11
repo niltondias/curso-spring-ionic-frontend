f61d2d670efe00f5f5d324b21a512eaf53999b61
Nilton Dias <ntjdias@gmail.com>
2020-04-11 19:24:24 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -54,14 +55,18 @@ export class OrderConfirmationPage {
   }
 
   back() {
-    this.navCtrl.setRoot('CartPage');
+    this.navCtrl.setRoot('CartPage'); 
+  }
+  
+  home() {
+    this.navCtrl.setRoot('CategoriasPage'); 
   }
 
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe( response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if( error.status == 403 ) {
@@ -69,6 +74,11 @@ export class OrderConfirmationPage {
         }
       });
 
+  }
+
+  private extractId( location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
