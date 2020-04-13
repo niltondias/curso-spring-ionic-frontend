8e1a2dd0f7b11dc42da3462b7b2c638c4839df11
Nilton Dias <ntjdias@gmail.com>
2020-04-13 17:28:45 -0300

    Obtendo imagem da galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -75,11 +75,31 @@ export class ProfilePage {
     });
   }
 
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
   sendPicture() {
     this.clienteService.uploadPicture(this.picture)
       .subscribe( response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       },
       error => {} );
 
