1212fb95a4183208a755aa77290ee18486e5c93e
Nilton Dias <ntjdias@gmail.com>
2020-04-13 16:03:08 -0300

    Testando o app no dispositivo

--- src/config/api.cofig.ts
+++ src/config/api.cofig.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://nilton-curso-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://nilton-curso-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
